faf29c54464fe670ae35b2be72f2e51f2cdac5bd
Tianran <tr.wang236@gmail.com>
2018-06-23 13:17:05 +0200

    generate motor commands

--- src/QuadControl.cpp
+++ src/QuadControl.cpp
@@ -70,10 +70,26 @@ VehicleCommand QuadControl::GenerateMotorCommands(float collThrustCmd, V3F momen
 
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
-  cmd.desiredThrustsN[0] = mass * 9.81f / 4.f; // front left
-  cmd.desiredThrustsN[1] = mass * 9.81f / 4.f; // front right
-  cmd.desiredThrustsN[2] = mass * 9.81f / 4.f; // rear left
-  cmd.desiredThrustsN[3] = mass * 9.81f / 4.f; // rear right
+    const float l = L / sqrt(2.0F);
+
+    float Ft = collThrustCmd;
+
+    // force * radius = moment
+    float Fp = momentCmd.x / l;
+    float Fq = momentCmd.y / l;
+    // force * kappa = moment
+    float Fr = momentCmd.z / kappa;
+
+    // 4 equations -> solved for four variables
+    float F1 = (Ft + Fp + Fq - Fr)/4;
+    float F2 = F1 - (Fp - Fr)/2;
+    float F4 = (Ft - Fp)/2 - F2;
+    float F3 = Ft - F1 - F2 - F4;
+
+    cmd.desiredThrustsN[0] = CONSTRAIN(F1, minMotorThrust, maxMotorThrust); // front left
+    cmd.desiredThrustsN[1] = CONSTRAIN(F2, minMotorThrust, maxMotorThrust); // front right
+    cmd.desiredThrustsN[2] = CONSTRAIN(F3, minMotorThrust, maxMotorThrust); // rear left
+    cmd.desiredThrustsN[3] = CONSTRAIN(F4, minMotorThrust, maxMotorThrust); // rear right
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
